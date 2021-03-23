69d40e506bf70f6f38a15140577c8a665bc4e854
m215592 <61302393+m215592@users.noreply.github.com>
2021-03-23 15:12:02 -0400

    Update hash.py

--- hash.py
+++ hash.py
@@ -1,3 +1,26 @@
+#no clue if this works:
 
-hello
-world
+import sys
+import os
+import hashlib
+
+#setting up the hashing function
+
+
+# BUF_SIZE is totally arbitrary, change for your app!
+BUFFER_SIZE = 65536  # lets read stuff in 64kb chunks!
+
+for filename in os.listdir(directory):
+  sha256 = hashlib.sha256()
+  with open(filename, 'rb') as file:
+      while True:
+          data = file.read(BUFFER_SIZE)
+          if not data:
+              break
+          sha256.update(data)
+
+  print("SHA256: {0}".format(sha256.hexdigest()))
+
+
+
+  
