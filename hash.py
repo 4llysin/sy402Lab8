bfcb43ad24e9034127b8cc83b3fe73b7cb25a9cf
4llysin <61302199+4llysin@users.noreply.github.com>
2021-04-06 22:11:00 -0400

    Update hash.py

--- hash.py
+++ hash.py
@@ -1,12 +1,24 @@
-#no clue if this works:
-
 import sys
 import os
 import hashlib
 
-#setting up the hashing function
+#things to ignore: 
+ignoreList = ["/usr", "/boot", "/bin", "/etc", "/dev", "/proc", "/run", "/sys", "/tmp", "/var/lib", "/var/run"]
 
 
+#if
+#open csv for reading
+#split csv by commas
+#close csv
+
+#if file exists, open for read, close
+#if it doesnt, open and write
+
+#OS.WALK??? if any files in ignored dict, continue, 
+#else: file gets date and time and gets hashed
+
+
+#set up hash function
 # BUF_SIZE is totally arbitrary, change for your app!
 BUFFER_SIZE = 65536  # lets read stuff in 64kb chunks!
 
@@ -21,6 +33,9 @@ for filename in os.listdir(directory):
 
   print("SHA256: {0}".format(sha256.hexdigest()))
 
+
+#compare hashes
+#print statement if difference is found
 
 
   
